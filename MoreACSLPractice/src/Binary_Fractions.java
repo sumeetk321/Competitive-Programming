16e36420744f18cd8ca2f7352cadec1bbb68237a
sumeetk321 <sumeetk321@yahoo.com>
2020-07-08 14:14:37 -0700

    Add files via upload

---
+++ MoreACSLPractice/src/Binary_Fractions.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Binary_Fractions {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] temp = br.readLine().split(", ");
+		
+
+	}
+
+}
